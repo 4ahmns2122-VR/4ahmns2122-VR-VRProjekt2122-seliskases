99d7752862e891b87af0f804a1ef1f8d3c952f82
simoneliskases <simon.eliskases@outlook.de>
2022-01-13 13:42:11 +0100

    Set Player Trigger on Chess Board

--- Assets/MyGame/Scripts/Puzzles/Chess/PuzzleTrigger.cs
+++ Assets/MyGame/Scripts/Puzzles/Chess/PuzzleTrigger.cs
@@ -1,23 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Chess;
 
 public class PuzzleTrigger : MonoBehaviour
 {
     public GameObject boardContainer;
-
-    private void Start()
-    {
-        boardContainer.SetActive(false);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             boardContainer.SetActive(true);
-            new Board();
         }
     }
 }
